d2676dc74d406f6c890fc78efcb047f86ed77807
JohnOkari <johnnokari@gmail.com>
2022-11-18 08:52:07 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ main.remove(main);
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "John is the champion!";
-newHeader.className = "victory";
+newHeader.className = "John";
